4a896b67cc2ea554746fce9c7c36aa743281feb3
zakariasan <elitziko@gmail.com>
2025-02-14 21:09:24 +0100

    add Issues component and style nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { PiCoffeeFill } from "react-icons/pi";
+import { usePathname } from "next/navigation";
 
 export const NavBar = () => {
   const links = [
@@ -14,15 +16,18 @@ export const NavBar = () => {
     },
   ];
 
+  const path = usePathname();
   return (
     <nav className="flex space-x-6 mb-5 border-b h-16 px-5 items-center ">
       <Link href="/">
         <PiCoffeeFill />
-
       </Link>
-      <ul className="flex space-x-6" >
+      <ul className="flex space-x-6">
         {links.map((link) => (
-          <li className="text-zinc-500 hover:text-zinc-900 transition-colors" key={link.item}>
+          <li
+            className={`${link.href === path ? " text-zinc-900" : " text-zinc-500"} text-zinc-500 hover:text-zinc-900 transition-colors`}
+            key={link.item}
+          >
             <Link href={link.href}>{link.item}</Link>
           </li>
         ))}
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const issues = () => {
+  return (
+    <div> hello from issues</div>
+  )
+}
+
+export default issues
