f696cc0941119dfd6884cc9b2bcd8db5a93f89fa
zakariasan <elitziko@gmail.com>
2025-02-15 02:59:00 +0100

    get stuck here

--- app/issues/new_issue/page.tsx
+++ app/issues/new_issue/page.tsx
@@ -3,14 +3,38 @@ import { TextField, TextArea, Button } from "@radix-ui/themes";
 import React from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssue = () => {
+  
+  const router  = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-4">
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Describe the issue…" />
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push('/issues')
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Describe the issue…" {...field} />
+        )}
+      />
       <Button>Submit New Issue </Button>
-    </div>
+    </form>
   );
 };
 
