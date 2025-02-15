e6e5111ddf5ae2ddb4f5cad5f43b571ab3602a2b
zakariasan <elitziko@gmail.com>
2025-02-15 01:45:19 +0100

    add makeDown editor in new Issue

--- app/issues/new_issue/page.tsx
+++ app/issues/new_issue/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import { TextField, TextArea, Button } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
     <div className="max-w-xl space-y-4">
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Describe the issue…" />
+      <SimpleMDE placeholder="Describe the issue…" />
       <Button>Submit New Issue </Button>
     </div>
   );
