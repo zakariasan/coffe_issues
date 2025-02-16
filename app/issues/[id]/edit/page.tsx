69d196906d9600f140a559e487929b67267e9833
zakariasan <elitziko@gmail.com>
2025-02-16 18:02:54 +0100

    add Edit issue and refactor issueForm

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,17 @@
+import IssueForm from "@/app/components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+const editIssuePage = async ({ params }: Props) => {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) return notFound();
+  return <IssueForm />;
+};
+
+export default editIssuePage;
--- app/issues/new_issue/page.tsx
+++ app/issues/new_issue/page.tsx
@@ -1,62 +1,6 @@
-"use client";
-import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
-import { InfoCircledIcon } from "@radix-ui/react-icons";
-import React, { useState } from "react";
-import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { setErrorMap } from "zod";
-import dynamic from 'next/dynamic';
-
-interface IssueForm {
-  title: string;
-  description: string;
-}
-
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false,
-});
-
+import IssueForm from "@/app/components/IssueForm";
 const NewIssue = () => {
-  const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-
-  const [error, setError] = useState("");
-  return (
-    <div className="max-w-xl">
-      {error && (
-        <Callout.Root size="3" color="red">
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form
-        className=" space-y-4"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setError("Title and description are required!");
-          }
-        })}
-      >
-        <TextField.Root placeholder="Title" {...register("title")} />
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Describe the issue…" {...field} />
-          )}
-        />
-        <Button>Submit New Issue </Button>
-      </form>
-    </div>
-  );
+  return <IssueForm />;
 };
 
 export default NewIssue;
