0a3081506e338b46a2b58ca4cdfd7d8c6cd13864
zakariasan <elitziko@gmail.com>
2025-02-17 03:37:04 +0100

    ssr problem

--- app/issues/new_issue/page.tsx
+++ app/issues/new_issue/page.tsx
@@ -2,7 +2,6 @@
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./Loading";
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
-  ssr: false,
   loading: () => <IssueFormSkeleton />,
 });
 const NewIssue = () => {
